8ae6ae5d8a8f6374f75cf72704ec8641c35abe56
Laima <kulbyte.laima@gmail.com>
2025-04-03 17:58:59 +0300

    update CSS

--- front/src/components/AddTicketForm.jsx
+++ front/src/components/AddTicketForm.jsx
@@ -35,93 +35,85 @@ const AddTicketForm = () => {
 
   return (
     <>
-    <div
-      className="flex flex-col items-center h-screen text-center bg-cover bg-no-repeat bg-center "
-      style={{ backgroundImage: `url(${Background})` }}
-    >
-      <img src={Line2} alt="" className="absolute top-0 right-0"/>
-      <div className="w-full max-w-md p-6 absolute z-20">
-        <div className="flex flex-col items-center mb-6">
-          <img src={Logo} alt="" className="pt-6"/>
+      <div
+        className="flex flex-col items-center h-screen text-center bg-cover bg-no-repeat bg-center "
+        style={{ backgroundImage: `url(${Background})` }}
+      >
+        <img src={Line2} alt="" className="absolute top-0 right-0" />
+        <div className="w-full max-w-md p-6 absolute z-20">
+          <div className="flex flex-col items-center mb-6">
+            <img src={Logo} alt="logo" className="pt-6" />
 
-          <h1 className="text-3xl font-bold mt-2 text-white pt-10 ">
-            Your Journey to Coding Conf 2025 Starts Here!
-          </h1>
-        </div>
+            <h1 className="text-3xl font-bold mt-2 text-white pt-10 ">
+              Your Journey to Coding Conf 2025 Starts Here!
+            </h1>
+          </div>
 
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="w-full space-y-8 pt-3  "
-        >
-          <section>
-            <input
-             
-              {...register("full_name", {
-                required: "Full name is required",
-                maxLength: { value: 100, message: "Max 100 characters" },
-              })}
-              type="text"
-              placeholder="Full name"
-              className="border border-white w-full placeholder:text-white rounded-md p-2 text-white"
-              onInput={() => setError("")}
-            />
-            <p className="text-peach text-sm">{errors.full_name?.message}</p>
-          </section>
-
-          <section>
-            <input
-             
-              {...register("email", {
-                required: "Email is required",
-                pattern: {
-                  value:
-                    /^[\w]([\w]+[\.\-]*)+[\w]@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                  message: "Invalid email address",
-                },
-                maxLength: { value: 255, message: "Max 255 characters" },
-              })}
-              type="text"
-              placeholder="Email"
-              className="border border-white placeholder:text-white rounded-md p-2 w-full text-white"
-              onInput={() => setError("")}
-            />
-            <p className="text-peach text-sm">{errors.email?.message}</p>
-          </section>
-
-          <section>
-            <input
-             
-              {...register("github_username", {
-                required: "GitHub username is required",
-                pattern: {
-                  value: /^@[a-zA-Z\d](?:[a-zA-Z\d]|-(?=[a-zA-Z\d])){0,98}$/,
-                  message: "Invalid GitHub username",
-                },
-                maxLength: { value: 100, message: "Max 100 characters" },
-              })}
-              type="text"
-              placeholder="GitHub username"
-              className="border border-white w-full placeholder:text-white rounded-md p-2 text-white"
-            />
-            <p className="text-peach text-sm">
-              {errors.github_username?.message}
-            </p>
-          </section>
-
-          <button
-            className="p-2 rounded-md w-full bg-peach"
-            type="submit"
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="w-full space-y-8 pt-3  "
           >
-            Generate My Ticket
-          </button>
-          {error && <p className="text-peach text-sm">{error}</p>}
-        </form>
+            <section>
+              <input
+                {...register("full_name", {
+                  required: "Full name is required",
+                  maxLength: { value: 100, message: "Max 100 characters" },
+                })}
+                type="text"
+                placeholder="Full name"
+                className="border border-white w-full placeholder:text-white rounded-md p-2 text-white"
+                onInput={() => setError("")}
+              />
+              <p className="text-peach text-sm">{errors.full_name?.message}</p>
+            </section>
+
+            <section>
+              <input
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value:
+                      /^[\w]([\w]+[\.\-]*)+[\w]@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Invalid email address",
+                  },
+                  maxLength: { value: 255, message: "Max 255 characters" },
+                })}
+                type="text"
+                placeholder="Email"
+                className="border border-white placeholder:text-white rounded-md p-2 w-full text-white"
+                onInput={() => setError("")}
+              />
+              <p className="text-peach text-sm">{errors.email?.message}</p>
+            </section>
+
+            <section>
+              <input
+                {...register("github_username", {
+                  required: "GitHub username is required",
+                  pattern: {
+                    value: /^@[a-zA-Z\d](?:[a-zA-Z\d]|-(?=[a-zA-Z\d])){0,98}$/,
+                    message: "Invalid GitHub username",
+                  },
+                  maxLength: { value: 100, message: "Max 100 characters" },
+                })}
+                type="text"
+                placeholder="GitHub username"
+                className="border border-white w-full placeholder:text-white rounded-md p-2 text-white"
+              />
+              <p className="text-peach text-sm">
+                {errors.github_username?.message}
+              </p>
+            </section>
+
+            <button className="p-2 rounded-md w-full bg-peach" type="submit">
+              Generate My Ticket
+            </button>
+            {error && <p className="text-peach text-sm">{error}</p>}
+          </form>
         </div>
       </div>
-      <img src={Line} alt="" className="absolute bottom-0 left-0 z-10"/>
-    
-     
-              </>
+      <img src={Line} alt="" className="absolute bottom-0 left-0 z-10" />
+    </>
   );
 };
 
--- front/src/components/Ticket.jsx
+++ front/src/components/Ticket.jsx
@@ -1,4 +1,3 @@
-import { RiDiscFill } from "react-icons/ri";
 import React, { useContext } from "react";
 import TicketContext from "../../context/TicketContext";
 import { useNavigate } from "react-router";
@@ -20,55 +19,79 @@ const Ticket = () => {
   return (
     <>
       <div
-        className="h-screen bg-cover bg-no-repeat bg-center flex justify-center items-center flex-col"
+        className="h-screen bg-cover bg-no-repeat bg-center flex justify-center items-center flex-col relative px-4"
         style={{ backgroundImage: `url(${Background})` }}
       >
-        <img src={Line2} className="absolute top-0 right-0" alt="" />
-        <img src={Line} className="absolute bottom-0 left-0" alt="" />
-        <img src={Logo} alt="" className="absolute top-9 w-40" />
-        <p className="text-white text-4xl absolute top-25 text-center">
+        <img
+          src={Line2}
+          className="absolute top-0 right-0 w-24 sm:w-auto"
+          alt="line2"
+        />
+        <img
+          src={Line}
+          className="absolute bottom-0 left-0 w-24 sm:w-auto"
+          alt="line"
+        />
+
+        <img
+          src={Logo}
+          alt="Logo"
+          className="absolute top-4 sm:top-9 w-50 sm:w-70"
+        />
+
+        <p className="text-white text-2xl sm:text-4xl md:mt-24 text-center z-10">
           Congrats, <span className="text-peach">{ticket.full_name}</span>!
         </p>
-        <p className="text-white text-4xl absolute top-37 text-center">
+        <p className="text-white text-2xl sm:text-4xl mt-4 text-center z-10">
           Your ticket is ready.
         </p>
-        <div className="w-80 text-center px-4">
-          <p className="text-white font-sans pt-30 pb-10">
-            We've emailed your ticket to 
+
+        <div className="max-w-md text-center mt-8 px-2 sm:px-4 z-10">
+          <p className="text-white font-sans text-base sm:text-lg">
+            We've emailed your ticket to
             <span className="text-peach"> {ticket.email}</span> and will send
             updates in the run-up to the event.
           </p>
         </div>
-        
-        
-        <div className="relative max-w-sm md:max-w-md lg:max-w-lg mx-auto overflow-hidden">
+
+        <div className="relative max-w-sm md:max-w-md lg:max-w-lg mx-auto mt-4 mb-4">
           <img src={TicketPattern} alt="" className="w-full h-auto" />
-          <div className="absolute top-6 left-2 flex flex-col items-center justify-center text-center px-4">
+          <div className="absolute top-6 left-2 flex flex-col items-center justify-center text-center px-4 w-full pr-30">
             <img src={Logo} alt="" className="w-50 md:w-70" />
 
             <p className="text-white tracking-wide font-sans text-sm">
               Jan 31, 2025 / Austin, TX
             </p>
 
-            <p className="text-white text-lg md:text-2xl pt-7 md:pt-9 lg:pt-15">{ticket.full_name}</p>
-            
-            <div className="flex items-center justify-center mt-2">
-              <img src={Github} alt="" className="w-6 h-6" />
-              <p className="text-white tracking-wide font-sans text-sm  ml-2">
-                {ticket.github_username}
-              </p>
+            <p className="text-white text-lg md:text-2xl pt-7 md:pt-9 lg:pt-15">
+              {ticket.full_name}
+            </p>
+
+            <div className="flex flex-col items-center mb-4">
+              <div className="flex items-center justify-center">
+                <img src={Github} alt="" className="w-6 h-6" />
+                <p className="text-white tracking-wide font-sans text-sm ml-2">
+                  {ticket.github_username}
+                </p>
+              </div>
             </div>
 
-            <p className="text-white text-xl md:text-2xl transform rotate-90 absolute left-78 md:left-90 lg:left-106 ">
+            <p
+              className="absolute text-white text-xl md:text-2xl lg:text-3xl rotate-90"
+              style={{
+                top: "59%",
+                left: "77%",
+                transform: "translate(-50%, -50%)",
+              }}
+            >
               {ticket.ticket_id}
             </p>
           </div>
         </div>
 
-        
         <button
           onClick={() => navigate("/")}
-          className="pt-2 w-40 bg-peach p-2 rounded-md absolute bottom-1/20"
+          className="pt-2 w-40 bg-peach p-2 rounded-md mt-4 mb-4"
         >
           Back
         </button>
